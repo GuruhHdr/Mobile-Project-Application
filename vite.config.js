93507aa3e2f2f3725df0f6ba039dec010f9e4001
GuruhHdr <hendryan.g017@gmail.com>
2024-10-24 11:05:56 +0700

    coba 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 export default defineConfig({
   base: "/Mobile-Project-Application/", // Ganti dengan nama repositori Anda
   plugins: [react()],
-  plugins: [ghPages()],
 });
 
 
